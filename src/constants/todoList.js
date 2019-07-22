7f6ea8f74162c13ac716b24d1a8f284052fa0909
adam <adam.yh.dong@gmail.com>
2019-07-22 21:57:09 +0800

    Modified to do list data.

--- src/constants/todoList.js
+++ src/constants/todoList.js
@@ -1,11 +1,11 @@
 const todoData = [
-    {id:1,name:'資信面試',date:'2018-10-20',time:'23:00',important:'Y',complete:false},
-    {id:2,name:'World Gym 健身',date:'2018-10-19',time:'20:00',important:'',complete:false},
-    {id:3,name:'聚餐',date:'2018-10-19',time:'20:00',important:'',complete:false},
-    {id:4,name:'Learn React',date:'2018-10-19',time:'20:00',important:'',complete:false},
-    {id:5,name:'串接API',date:'2018-10-19',time:'20:00',important:'',complete:false},
-    {id:6,name:'Test my todos',date:'2018-10-19',time:'20:00',important:'',complete:false},
-    {id:7,name:'寫履歷',date:'2018-10-19',time:'20:00',important:'',complete:false},
+    {id:1,name:'準備面試 Demo',date:'2019-07-22',time:'23:00',important:'Y',complete:false},
+    {id:2,name:'World Gym 健身',date:'2018-07-22',time:'20:00',important:'',complete:false},
+    {id:3,name:'聚餐',date:'2019-07-25',time:'20:00',important:'',complete:false},
+    {id:4,name:'Learn React',date:'2019-07-19',time:'20:00',important:'',complete:false},
+    {id:5,name:'串接API',date:'2019-07-22',time:'20:00',important:'',complete:false},
+    {id:6,name:'Test my todos',date:'2019-07-21',time:'20:00',important:'',complete:false},
+    {id:7,name:'寫履歷',date:'2019-07-21',time:'20:00',important:'',complete:false},
 ]
 
 export {todoData}
