2880dfe3ba9facd831826b0797cdc26e0e08e167
adam <adam.yh.dong@gmail.com>
2019-07-22 21:53:00 +0800

    Modified code using ES5.

--- src/actions/todoList.js
+++ src/actions/todoList.js
@@ -1,10 +1,11 @@
 import {ADD_TODOLIST} from "../constants/todoAction-type.js"
 import {EDIT_TODOLIST} from "../constants/todoAction-type.js"
 
-export const addTodoList = todoList => ({
-    type : ADD_TODOLIST,
-    payload : todoList
-})
+export function addTodoList(todoList){
+    return {
+        type : ADD_TODOLIST,
+        payload : todoList}
+}
 
 export const editTodoList = todoList => ({
     type : EDIT_TODOLIST,
