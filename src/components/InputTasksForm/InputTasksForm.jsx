7151b082d478d5571b989de5edc15e9c4b1d03b6
adam <adam.yh.dong@gmail.com>
2019-07-22 21:58:38 +0800

    Modified css.

--- src/components/InputTasksForm/InputTasksForm.jsx
+++ src/components/InputTasksForm/InputTasksForm.jsx
@@ -3,7 +3,7 @@ import React from "react"
 class InputTasksForm extends React.Component {
     render() {
         return (
-            <div>
+            <div class="saveButtons">
                 <div>
                     <button type="button" class="addButton saveButton" onClick={this.props.submitTodo}>Save</button>
                     <button type="button" class="addButton cancelButton" onClick={this.props.closeAdd}>Cancel</button>
